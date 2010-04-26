23951aa2eb7ab31a69c0b629f588db0b8bf4eeab
Vasily Polovnyov <vast@whiteants.net>
2010-04-26 12:30:19 +0400

    working page-fragments caching, great speed up

--- src/main/scala/page.scala
+++ src/main/scala/page.scala
@@ -19,11 +19,22 @@ class Page(val uri: String, var content: String) {
     writeStringToFile(file, content, "UTF-8")
   }
 
-  def cache() = writeStringToFile(new File(cachedPath), Markdown(content), "UTF-8")
+  def cache_!() = writeStringToFile(new File(cachedPath), Markdown(content), "UTF-8")
 
-  def sweep() = new File(cachedPath).delete
+  def sweep_!() = new File(cachedPath).delete
 
-  def toHtml() = Markdown(content)
+  def toHtml() = {
+    if (Page.caching_?) {
+      val f = new File(path)
+      val cf = new File(cachedPath)
+      if (!cf.exists || f.lastModified > cf.lastModified)
+        cache_!
+
+      readFileToString(cf, "UTF-8")
+    } else {
+      Markdown(content)
+    }
+  }
 
 }
 
@@ -35,6 +46,12 @@ object Page {
   val mdTitle = Pattern.compile("(^#\\s*?(\\S.*?)#*$)|(^ {0,3}(\\S.*?)\\n=+(?=\\n+|\\Z))",
     Pattern.MULTILINE)
   val password = Circumflex.cfg("ciridiri.password").getOrElse("pass")
+
+  def caching_?(): Boolean = Circumflex.cfg("ciridiri.caching").getOrElse(true) match {
+    case b: Boolean => b
+    case s: String => s.toBoolean
+    case _ => true
+  }
 
   def pathFromUri(uri: String) = FilenameUtils
       .concat(contentDir, (FilenameUtils.separatorsToSystem(uri) + sourceExt)
