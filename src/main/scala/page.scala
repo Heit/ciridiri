bf49d67bc58b0cc96640f3fce9e922696ca27e50
Vasily Polovnyov <vast@whiteants.net>
2010-05-17 13:16:12 +0400

    ciridiri.rememberMe must be configurable outside app

--- src/main/scala/page.scala
+++ src/main/scala/page.scala
@@ -47,7 +47,7 @@ object Page {
       "(^ {0,3}(\\S.*?)(?: *\\{#.*?\\})?\\n(?:=|-)+(?=\\n+|\\Z))",
     Pattern.MULTILINE)
   val password = Circumflex("ciridiri.password").getOrElse("pass")
-  val rememberMe = Circumflex("ciridiri.rememberMe").getOrElse(true) match {
+  var rememberMe = Circumflex("ciridiri.rememberMe").getOrElse(true) match {
     case b: Boolean => b
     case s: String => s.toBoolean
     case _ => true
