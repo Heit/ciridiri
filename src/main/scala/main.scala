2b21fb28a27631a371e017439e65272cf5c7232f
inca <incarnate@whiteants.net>
2010-05-18 17:52:49 +0800

    Fixed routes to use + splats instead of *

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,7 +7,7 @@ class CiriDiri extends RequestRouter with AuthHelper{
 
   get("/") = redirect("/index.html")
 
-  get("*.html") = Page.findByUri(uri(1)) match {
+  get("+.html") = Page.findByUri(uri(1)) match {
     case Some(page) =>
       "ciripage" := page
       ftl("/ciridiri/page.ftl")
@@ -15,7 +15,7 @@ class CiriDiri extends RequestRouter with AuthHelper{
       redirect(uri(0) + ".e")
   }
 
-  get("*.md") = Page.findByUri(uri(1)) match {
+  get("+.md") = Page.findByUri(uri(1)) match {
     case Some(page) =>
       contentType("text/plain; charset=utf-8")
       page.content
@@ -23,12 +23,12 @@ class CiriDiri extends RequestRouter with AuthHelper{
       error(404, "Page not found")
   }
 
-  get("*.html.e") = {
+  get("+.html.e") = {
     "ciripage" := Page.findByUriOrEmpty(uri(1))
     ftl("/ciridiri/edit.ftl")
   }
 
-  post("*.html") = {
+  post("+.html") = {
     protected_!
     var page = Page.findByUriOrEmpty(uri(1))
     page.content = param('content)
