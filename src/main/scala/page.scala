25c458f58c29e0394651484866ea80792c4f7aa2
inca <incarnate@whiteants.net>
2010-05-21 10:48:01 +0400

    Changed `toString` to return proper md-style link.

--- src/main/scala/page.scala
+++ src/main/scala/page.scala
@@ -36,7 +36,7 @@ class Page(val uri: String, var content: String) {
     Markdown(content)
   }
 
-  override def toString = "[" + title + "](" + cachedPath + ")"
+  override def toString = "[" + title + "](" + uri + ".html)"
 }
 
 object Page {
