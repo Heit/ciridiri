16c194592408cc8826983bfe196f6adb5b1586ae
Vasily Polovnyov <vast@whiteants.net>
2010-05-21 12:41:44 +0400

    merged with inca's found/notfound callbacks

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,23 +3,27 @@ package ru.ciridiri
 import ru.circumflex.core._
 import ru.circumflex.freemarker.FTL._
 
-class CiriDiri extends RequestRouter with AuthHelper{
+class CiriDiri extends RequestRouter with AuthHelper {
 
   get("/") = redirect("/index.html")
 
   get("+.html") = Page.findByUri(uri(1)) match {
     case Some(page) =>
+      onFound(page)
       "ciripage" := page
       ftl("/ciridiri/page.ftl")
     case None =>
+      onNotFound()
       redirect(uri(0) + ".e")
   }
 
   get("+.md") = Page.findByUri(uri(1)) match {
     case Some(page) =>
+      onFound(page)
       contentType("text/plain; charset=utf-8")
       page.content
-    case None =>
+    case _ =>
+      onNotFound()
       error(404, "Page not found")
   }
 
@@ -35,5 +39,20 @@ class CiriDiri extends RequestRouter with AuthHelper{
     page.save
     redirect(uri(0))
   }
+
+  // ## Callbacks
+
+  // Callbacks can be used to run user code in response to various CiriDiri events.
+  // Usage is simple:
+  //
+  //     class Main extends RequestRouter {
+  //       // some routing...
+  //       new CiriDiri {
+  //         override def onFound(page: Page) = println("I found the page " + page)
+  //       }
+  //     }
+
+  def onFound(page: Page): Unit = {}
+  def onNotFound(): Unit = {}
 
 }
